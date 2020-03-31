462e88056e252d56613c3d940eaa8b20664bac49
root <root@master.puppet.vm>
2020-03-31 21:29:03 +0000

    Added params to minecraft module

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,8 +1,11 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+  $install_dir = '/opt/minecraft'
+}{
+  file {$install_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/server.jar':
+  file {"${install_dir}/server.jar":
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
     before => Service['minecraft'],
@@ -10,13 +13,15 @@ class minecraft {
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
   file {'/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    ensure  => file,
+    content => epp('minecraft/minecraft.service', {
+      install_dir => $install_dir,
+    }),
   }
   service {'minecraft':
     ensure  => running,
