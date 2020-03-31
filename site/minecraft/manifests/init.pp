4ad2fd6d3edba00ca7aacbcb20f8d91226e3e377
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2020-03-31 16:55:02 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,7 +7,7 @@ class minecraft (
   }
   file {"${install_dir}/server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    source => $url,
     before => Service['minecraft'],
   }
   package {'java':
@@ -26,6 +26,6 @@ class minecraft (
   service {'minecraft':
     ensure  => running,
     enable  => true,
-    require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']],
   }
 }
